a11f1e1f2611b6404bf409be5ba13584d3f957c0
thebl3mish <rodrigo@plethora.com.mx>
2025-01-24 17:14:58 -0600

    chore: adds currency and period atoms.

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,11 +1,13 @@
 import { atom } from 'jotai'
-import { fetchCurrencyGraph, formatGraphData, formatLabels } from '../services/bitsoApi'
+import { fetchCurrencyGraph, formatGraphData, formatLabels, PreferredCurrency, Period } from '../services/bitsoApi'
 
 // Base atoms
 export const selectedCryptoAtom = atom('ETH')
 export const cryptoDataAtom = atom<number[]>([])
 export const cryptoLabelsAtom = atom<string[]>([])
 export const loadingAtom = atom(false)
+export const currencyAtom = atom<PreferredCurrency>('mxn')
+export const periodAtom = atom<Period>('1M')
 export const errorAtom = atom<string | null>(null)
 
 // Derived atom for fetching data
@@ -15,7 +17,9 @@ export const fetchCryptoDataAtom = atom(
     set(loadingAtom, true)
     set(errorAtom, null)
     try {
-      const rawData = await fetchCurrencyGraph(crypto.toLowerCase(), '1M', 'mxn')
+      const currency = get(currencyAtom)
+      const period = get(periodAtom)
+      const rawData = await fetchCurrencyGraph(crypto.toLowerCase(), period, currency)
       set(cryptoDataAtom, formatGraphData(rawData))
       set(cryptoLabelsAtom, formatLabels(rawData))
     } catch (error) {
