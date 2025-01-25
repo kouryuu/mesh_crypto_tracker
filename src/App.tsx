702c5a084780adf30a6a02fe5965a679c379c9a6
thebl3mish <rodrigo@plethora.com.mx>
2025-01-24 19:33:47 -0600

    feat: Updates main page using suspense for fallback handling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Suspense } from 'react'
+import { useAtom, useAtomValue} from 'jotai'
 import './App.css'
+import Controls from './components/Controls'
+import LineChart from './components/LineChart'
+import { 
+  selectedCryptoAtom, 
+  cryptoRawDataAtomCached,
+  errorAtom,
+  
+} from './store/atoms'
+import Loader from './components/Loader'
+import { formatGraphData, formatLabels } from './services/bitsoApi'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function ChartContent() {
+  const [selectedCrypto] = useAtom(selectedCryptoAtom)
+  const data = useAtomValue(cryptoRawDataAtomCached)
+  const error = useAtomValue(errorAtom)
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <LineChart 
+      dataLabels={formatLabels(data)} 
+      mainLabel={selectedCrypto} 
+      data={formatGraphData(data)} 
+    />
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <>
+      <Controls />
+      <Suspense fallback={<Loader />}>
+        <ChartContent />
+      </Suspense>
+    </>
+  )
+}
