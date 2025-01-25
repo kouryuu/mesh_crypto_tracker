7a7e806dd931bd75c68c071cba2b3a167cd6b8c8
thebl3mish <rodrigo@plethora.com.mx>
2025-01-24 19:28:11 -0600

    fix: removes annoying animation.

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -25,6 +25,13 @@ ChartJS.register(
 );
 
 export const options = {
+  animation: {
+    duration: 0 // general animation time
+  },
+  hover: {
+    animationDuration: 0 // duration of animations when hovering an item
+  },
+  responsiveAnimationDuration: 0, // animation duration after a resize
   elements: {
     line: {
       tension: 0.1,
